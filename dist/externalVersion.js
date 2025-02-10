7b248833cd6d5cb180da9e86ca36285dd395197d
buzz-code <hadasa.schechter@gmail.com>
2025-02-10 16:00:18 +0200

    update external version, v1.0.4

--- dist/externalVersion.js
+++ dist/externalVersion.js
@@ -1,16 +1,7 @@
-/**
- * This file is part of the NocoBase (R) project.
- * Copyright (c) 2020-2024 NocoBase Co., Ltd.
- * Authors: NocoBase Team.
- *
- * This project is dual-licensed under AGPL-3.0 and NocoBase Commercial License.
- * For more information, please refer to: https://www.nocobase.com/agreement.
- */
-
 module.exports = {
-  "@ant-design/icons": "5.5.2",
-  "@nocobase/plugin-workflow": "1.3.52",
-  "react": "18.3.1",
-  "@nocobase/client": "1.3.52",
-  "@nocobase/server": "1.3.52"
+  "@ant-design/icons": "5.2.6",
+  "@nocobase/plugin-workflow": "1.4.33",
+  "react": "18.2.0",
+  "@nocobase/client": "1.4.33",
+  "@nocobase/server": "1.4.33"
 };
